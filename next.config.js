ea984cf3a7a41358ba178c9122f8aa5ca7618857
rendystdy <rendysetiady8@gmail.com>
2024-11-11 14:27:00 +0700

    init comit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'rendy-nextjs-demo-users-image.s3.ap-southeast-2.amazonaws.com',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+}
+
+module.exports = nextConfig
